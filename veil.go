27cb3802df6bca01c7326ffd2e7d09640770dece
Marshall Nichols <marshall.nichols@duke.edu>
2019-02-10 21:10:03 -0500

    Update with JSON parsing experiments
    Add map and fixtures for testing json parsing both generically as well
    as into a very basic struct.  Long term plan is to use the Struct to
    house the user provided column map (with known structure) and the
    generic to house the raw data.
    
    We can eventually use this to setup goroutines to de-identify user
    defined arrays with some nice concurrency.  I still feel like a noob,
    but we're getting there.

--- veil.go
+++ veil.go
@@ -1,6 +1,60 @@
-package Veil
+//package Veil
+package main
+
+import ( 
+    "fmt"
+    "encoding/json"
+    "io/ioutil"
+    "os"
+)
+
+type VeilMapFmt struct {
+    Deidentify []string `json:"deidentify"`
+    Timeshift []string  `json:"timeshift"`
+}
+
+func VeilParseJSON(file string) map[string]interface{} {
+    jsonFile, err := os.Open(file)
+    if err != nil {
+        fmt.Println(err)
+    }
+    defer jsonFile.Close()
+
+    jsonValue, _ := ioutil.ReadAll(jsonFile)
+
+    var x map[string]interface{}
+    json.Unmarshal(jsonValue, &x)
+
+    return x
+}
+
+func VeilParseMap(file string, out VeilMapFmt) VeilMapFmt {
+    jsonFile, err := os.Open(file)
+    if err != nil {
+        fmt.Println(err)
+    }
+    defer jsonFile.Close()
+
+    jsonValue, _ := ioutil.ReadAll(jsonFile)
+
+    json.Unmarshal(jsonValue, &out)
+
+    return out
+}
 
 func VeilAdd(a, b float64) float64 {
     c := a + b
     return c
+}
+
+func main() {
+
+    var mp VeilMapFmt
+
+    out := VeilParseMap("veilMap.json", mp)
+    y := VeilParseJSON("veilFixture.json")
+    
+    fmt.Println(out.Deidentify)
+    fmt.Println(y["name"])
+
 }
